9e7bd0b356ba0cfedbc9cc4251e9ee0bd98ec41d
Flakoooo <115491068+Flakoooo@users.noreply.github.com>
2023-08-01 19:54:08 +0500

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,79 +1,17 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-import InputComp from '@Components/Input/InputComp.vue'
+import { storeToRefs } from 'pinia'
 
-let model = ref<string | number>('')
+import useIdeasStore from '@Store/ideas/ideasStore'
+import useUserStore from '@Store/user/userStore'
 
-let InpStyle = ref('idea-input')
+const userStore = useUserStore()
+const ideasStore = useIdeasStore()
 
-function changeInputStyle(style) {
-  InpStyle.value = style
-}
+const { user } = storeToRefs(userStore)
+const { ideas } = storeToRefs(ideasStore)
 </script>
 
 <template>
-  <div style="@flex (center,center); height: 150px">
-    <InputComp
-      inputType="text"
-      inputPlaceholder="пример placeholder'а"
-      v-model="model"
-      :inputStyle="InpStyle"
-    />
-    <div style="width: 50%; word-break: break-all; text-align: left">
-      {{ model }}
-    </div>
-  </div>
-  <div>
-    <button
-      type="button"
-      @click="changeInputStyle('idea-input')"
-    >
-      Стиль создания идеи
-    </button>
-    <button
-      type="button"
-      @click="changeInputStyle('login-input')"
-    >
-      Стиль логина
-    </button>
-  </div>
+  <pre v-if="user">Пользователь - {{ user }}</pre>
+  <pre v-if="ideas.length">{{ ideas }}</pre>
 </template>
-
-<style>
-.idea-input {
-  width: 50%;
-  height: 50%;
-  border: 1px solid #64b5f6;
-  border-radius: 4px;
-  font-size: 25px;
-  box-sizing: border-box;
-  padding-left: 16px;
-  padding-right: 16px;
-  box-shadow: 0px 0px 0px 4px #3184fd40;
-}
-.login-input {
-  width: 50%;
-  height: 50%;
-  border: 1px solid #2151ff;
-  border-radius: 12px;
-  font-size: 25px;
-  box-sizing: border-box;
-  padding-left: 16px;
-  padding-right: 16px;
-}
-
-.idea-input::placeholder,
-.login-input::placeholder {
-  color: #9a9a9a;
-  user-select: none;
-}
-.idea-input:focus,
-.login-input:focus {
-  outline: 2px solid transparent;
-  outline-offset: 2px;
-}
-.idea-input:focus::placeholder,
-.login-input:focus::placeholder {
-  color: transparent;
-}
-</style>
