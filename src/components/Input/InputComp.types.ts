a83ed4c031cbcbe2291febb550e88a35ae55504a
Flakoooo <vlasov.vadom@gmail.com>
2023-08-01 19:51:00 +0500

    Доработал Input компонент

---
+++ src/components/Input/InputComp.types.ts
@@ -0,0 +1,7 @@
+interface InputProps {
+  inputType: string
+  inputPlaceholder: string
+  inputStyle: string
+}
+
+export default InputProps
---
+++ src/components/Input/InputComp.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import InputProps from '@Components/Input/InputComp.types'
+const props = defineProps<InputProps>()
+const emit = defineEmits<{
+  (event: 'update:modelValue', payload: string): void
+}>()
+</script>
+
+<template>
+  <input
+    :type="props.inputType"
+    :placeholder="props.inputPlaceholder"
+    @input="
+      emit('update:modelValue', ($event.target as HTMLInputElement).value)
+    "
+    :class="props.inputStyle"
+  />
+</template>
+
+<style></style>
