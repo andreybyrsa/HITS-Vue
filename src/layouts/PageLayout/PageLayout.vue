0961d039f50592bc15c182f8fdd25b72977d67ac
kivaX9 <kirill.vlasov.05@inbox.tu>
2023-08-01 23:03:53 +0500

    Сделал компонент pageLayout

---
+++ src/layouts/PageLayout/PageLayout.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+defineProps({
+  leftSideBar: Boolean,
+})
+</script>
+
+<template>
+  <div class="page-layout vw-100">
+    <div
+      v-if="leftSideBar"
+      class="page-layout__leftsidebar"
+    >
+      <slot name="leftSideBar" />
+    </div>
+    <div class="page-layout__content">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.page-layout {
+  @include flexible(flex-start, flex-start);
+
+  &__leftsidebar {
+    @include position(sticky, $left: 0, $top: 0, $bottom: 0);
+    @include border(solid, #d8d8d8, 0, 1px);
+
+    min-width: 320px;
+    height: 100vh;
+  }
+
+  &__content {
+    @include flexible(center, center);
+    width: 100%;
+    height: 100vh;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,25 @@
 <script lang="ts" setup>
 import HelloWorld from '@Components/HelloWorld.vue'
+import PageLayout from '../layouts/PageLayout/PageLayout.vue'
 </script>
 
 <template>
-  <div class="home">
-    <img
-      alt="Vue logo"
-      src="../assets/images/logo.png"
-    />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <PageLayout leftSideBar>
+    <template v-slot:leftSideBar>
+      <h1>HITS Idea</h1>
+      <button>кнопка</button>
+      <button>кнопка</button>
+      <button>кнопка</button>
+    </template>
+
+    <template v-slot:content>
+      <img
+        alt="Vue logo"
+        src="../assets/images/logo.png"
+      />
+      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    </template>
+  </PageLayout>
 </template>
+
+<style scoped lang="scss"></style>
