41846ef368ecd671df195e9902ae560adc264c99
andreybyrsa <byrsa2004@bk.ru>
2023-08-01 12:04:27 +0500

    Добавил ESLint в проект

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,43 @@
+const path = require('path')
+
+const resolvePath = (currentPath) => path.resolve(__dirname, currentPath)
+
 module.exports = {
   root: true,
   env: {
     node: true,
   },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+  },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
+  plugins: ['eslint-plugin-prettier', 'eslint-plugin-import'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'import/no-unresolved': ['off', { commonjs: true }],
+    'import/no-extraneous-dependencies': 'error',
+    'node/no-missing-require': 'off',
+    'node/no-extraneous-import': 'off',
+
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-};
+}
