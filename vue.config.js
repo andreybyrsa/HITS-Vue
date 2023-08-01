5149c7caec9839806169683aebd74a2eecbe0f91
andreybyrsa <byrsa2004@bk.ru>
2023-08-01 12:05:19 +0500

    Настроил конфиг для проекта

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
+const resolvePath = (currentPath) => path.resolve(__dirname, currentPath)
+
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "./src/assets/styles/main.scss";',
+      },
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@Assets': resolvePath('src/assets/'),
+        '@Components': resolvePath('src/components/'),
+        '@Layouts': resolvePath('src/layouts/'),
+        '@Views': resolvePath('src/views/'),
+        '@Hooks': resolvePath('src/hooks/'),
+        '@Services': resolvePath('src/services'),
+        '@Middlewares': resolvePath('src/middlewares'),
+        '@Store': resolvePath('src/store/'),
+        '@Utils': resolvePath('src/utils/'),
+      },
+    },
+  },
+})
