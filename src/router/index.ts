4fda3a86808fb58a8bfc382103f2bc9caf1be443
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2023-08-03 22:38:27 +0500

    Добавил формы Забыл пароль и Новый пароль

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,21 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 import DevView from '@Views/DevView.vue'
+import FgPass from '@Views/ForgotPassword.vue'
+import NewPass from '@Views/NewPassword.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/dev',
     component: DevView,
+  },
+  {
+    path: '/fg_pass',
+    component: FgPass,
+  },
+  {
+    path: '/new_pass/:id',
+    component: NewPass,
   },
 ]
 
---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,51 @@
+<template>
+  <PageLayout content-class-name="pass-page__content">
+    <template #content>
+      <div
+        style="
+          width: 40%;
+
+          padding: 24px;
+          border-radius: 22px;
+          box-shadow: 2px 2px 40px 0px rgba(0, 0, 0, 0.25);
+        "
+        class="center"
+      >
+        <div style="margin-bottom: 32px">
+          <Typography class-name="fs-1 text-primary"
+            >Востановление пароля</Typography
+          >
+        </div>
+        <Input
+          placeholder="Введите emile"
+          label="Emile"
+          style="margin-bottom: 16px; border-color: #2151ff"
+        ></Input>
+        <Button
+          class-name="btn-primary"
+          style="margin-top: 24px"
+          >Отправить</Button
+        >
+      </div>
+    </template>
+  </PageLayout>
+</template>
+
+<script lang="ts" setup>
+import Button from '@Components/Button/Button.vue'
+import Input from '@Components/Input/Input.vue'
+import PageLayout from '@Layouts/PageLayout/PageLayout.vue'
+import Typography from '@Components/Typography/Typography.vue'
+</script>
+
+<style lang="scss">
+.pass-page {
+  &__content {
+    @include flexible(center, center, column, $gap: 16px);
+  }
+}
+
+.center {
+  @include flexible(start, center, column);
+}
+</style>
