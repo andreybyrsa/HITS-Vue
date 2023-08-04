1f2a8a113ec9c4cefa1c36ced4518853625e1f43
andreybyrsa <byrsa2004@bk.ru>
2023-08-04 13:57:22 +0500

    Сделал компонент DropDown

---
+++ src/components/DropDown/DropDown.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+import DropDownProps from './DropDown.types'
+
+const props = defineProps<DropDownProps>()
+
+const DropDownClassName = computed(() => ['collapse', props.className])
+</script>
+
+<template>
+  <div
+    :id="id"
+    :class="DropDownClassName"
+  >
+    <div class="list-group p-2">
+      <slot></slot>
+    </div>
+  </div>
+</template>
