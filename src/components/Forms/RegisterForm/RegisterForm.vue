db2e51d6ce4e8b8b388e53bd6b837d5c182511aa
andreybyrsa <byrsa2004@bk.ru>
2023-08-10 16:47:45 +0500

    Сделал валидацию для RegisterForm

--- src/components/Forms/RegisterForm/RegisterForm.vue
+++ src/components/Forms/RegisterForm/RegisterForm.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
-import { reactive, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import { useForm } from 'vee-validate'
 
 import Typography from '@Components/Typography/Typography.vue'
 import Input from '@Components/Inputs/Input/Input.vue'
@@ -15,41 +16,51 @@ import useUserStore from '@Store/user/userStore'
 
 import ManageUsersService from '@Services/ManageUsersService'
 
+import Validation from '@Utils/Validation'
+import RolesTypes from '@Domain/Roles'
+
 const userStore = useUserStore()
 const route = useRoute()
 
-const userData = reactive<RegisterUser>({
-  email: '',
-  firstName: '',
-  lastName: '',
-  password: '',
-  roles: [],
+const { setFieldValue, handleSubmit } = useForm<RegisterUser>({
+  validationSchema: {
+    email: (value: string) => Validation.checkEmail(value),
+    firstName: (value: string) =>
+      Validation.checkName(value) || 'Неверно введено имя',
+    lastName: (value: string) =>
+      Validation.checkName(value) || 'Неверно введена фамилия',
+    password: (value: string) => Validation.checkPassword(value),
+    roles: (value: RolesTypes[]) => value?.length,
+  },
 })
 
-function handleRegister(user: RegisterUser) {
-  userStore.registerUser(user)
-}
+const handleRegister = handleSubmit((values) => {
+  userStore.registerUser(values)
+})
 
 onMounted(async () => {
   const { email, roles } = await ManageUsersService.getInvitationInfo(
     route.params.slug,
   )
-  userData.email = email
-  userData.roles = roles
+  if (email && roles) {
+    setFieldValue('email', email)
+    setFieldValue('roles', roles)
+  }
 })
 </script>
 
 <template>
   <FormLayout>
-    <Typography class-name="fs-3 text-primary"> Регистрация </Typography>
+    <Typography class-name="fs-3 text-primary">Регистрация</Typography>
 
     <Input
       v-for="input in registerInputs"
       :key="input.key"
       :type="input.type"
-      v-model="userData[input.key]"
+      :name="input.name"
       :placeholder="input.placeholder"
       :prepend="input.prepend"
+      :disabled="input.disabled"
     >
       <template #prepend>
         <i :class="input.prependIcon"></i>
@@ -57,8 +68,9 @@ onMounted(async () => {
     </Input>
 
     <Button
+      type="submit"
       class-name="btn-primary w-100"
-      @click="handleRegister(userData)"
+      @click="handleRegister"
     >
       Зарегистрироваться
     </Button>
