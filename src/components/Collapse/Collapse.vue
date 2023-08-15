81e212c1ebf9c4a676e05d1f61f19c605f31b026
andreybyrsa <byrsa2004@bk.ru>
2023-08-15 14:22:29 +0500

    Сделал компонент Collapse

---
+++ src/components/Collapse/Collapse.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+import CollapseProps from '@Components/Collapse/Collapse.types'
+
+const props = defineProps<CollapseProps>()
+
+const CollapseClassName = computed(() => ['collapse', props.className])
+</script>
+
+<template>
+  <div
+    :id="id"
+    :class="CollapseClassName"
+  >
+    <div class="list-group p-2">
+      <slot></slot>
+    </div>
+  </div>
+</template>
