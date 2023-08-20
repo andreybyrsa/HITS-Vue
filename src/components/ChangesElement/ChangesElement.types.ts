6b7155240d3bc9b5257309530c6e2d206f7f48a4
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2023-08-20 18:16:31 +0500

    Страница журнала изменений

---
+++ src/components/ChangesElement/ChangesElement.types.ts
@@ -0,0 +1,7 @@
+interface NotificationModalProps {
+  text?: 'green' | 'red'
+  date?: string
+  type?: 'personal' | 'ideas' | 'system'
+}
+
+export { NotificationModalProps }
---
+++ src/components/ChangesElement/ChangesElement.vue
@@ -0,0 +1,73 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+import { NotificationModalProps } from '@Components/ChangesElement/ChangesElement.types'
+import Typography from '@Components/Typography/Typography.vue'
+
+const props = defineProps<NotificationModalProps>()
+
+const NotificationClassName = computed(() => {
+  switch (props.text) {
+    case 'green':
+      return ['card text-success shadow']
+    case 'red':
+      return ['card text-danger shadow']
+    default:
+      return ['card shadow']
+  }
+})
+
+const NotificationHeader = computed(() => {
+  switch (props.type) {
+    case 'personal':
+      return 'Персональное'
+    case 'ideas':
+      return 'Идеи'
+    case 'system':
+      return 'Система'
+    default:
+      return 'Изменение'
+  }
+})
+</script>
+
+<template>
+  <Transition name="notification-modal">
+    <div :class="NotificationClassName">
+      <div class="card-header">
+        <Typography class-name="text-light">{{
+          NotificationHeader
+        }}</Typography>
+        <Typography class-name="text-white-50 fs-6">{{ date }}</Typography>
+      </div>
+
+      <div class="card-body">
+        <slot></slot>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  width: 400px;
+  height: min-content;
+  margin: 16px;
+  margin-right: 100px;
+
+  transition: opacity $default-transition-settings;
+
+  &-header {
+    @include flexible(center, space-between);
+    background-color: #0d6efd;
+  }
+}
+
+.notification-modal-enter-from {
+  opacity: 0;
+}
+
+.notification-modal-leave-to {
+  opacity: 0;
+}
+</style>
