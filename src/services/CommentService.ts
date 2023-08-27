4cbdea1c7b4048a8732da4dd2c27600b183a254c
andreybyrsa <byrsa2004@bk.ru>
2023-08-27 19:40:14 +0500

    Сделал commentsStore

--- src/services/CommentService.ts
+++ src/services/CommentService.ts
@@ -5,13 +5,28 @@ import Comment from '@Domain/Comment'
 
 const COMMENT_URL = process.env.VUE_APP_COMMENT_API_URL || 'http://localhost:3000'
 
+const fetchIdeaComments = async (
+  ideaId: number,
+  token: string,
+): Promise<{ comments: Comment[] } | Error> => {
+  return await axios
+    .get(`${COMMENT_URL}/get-idea-comments/${ideaId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((response) => response.data)
+    .catch(({ response }) => {
+      const error = response?.data?.error ?? 'Ошибка получения комментариев'
+      return new Error(error)
+    })
+}
+
 const addComment = async (
   comment: Comment,
-  ideaID: number,
+  ideaId: number,
   token: string,
 ): Promise<Comment | Error> => {
   return await axios
-    .post(`${COMMENT_URL}/add/${ideaID}`, comment, {
+    .post(`${COMMENT_URL}/add/${ideaId}`, comment, {
       headers: { Authorization: `Bearer ${token}` },
     })
     .then((response) => response.data)
@@ -22,12 +37,11 @@ const addComment = async (
 }
 
 const deleteComment = async (
-  ideaID: number,
-  commentID: number,
+  commentId: number,
   token: string,
 ): Promise<Success | Error> => {
   return await axios
-    .delete(`${COMMENT_URL}/delete/${ideaID}/${commentID}`, {
+    .delete(`${COMMENT_URL}/delete/${commentId}`, {
       headers: { Authorization: `Bearer ${token}` },
     })
     .then((response) => response.data)
@@ -37,18 +51,20 @@ const deleteComment = async (
     })
 }
 
-const checkComment = async (ideaID: number, comment: Comment, token: string) => {
+const checkComment = async (commentId: number, token: string) => {
   return await axios
-    .put(`${COMMENT_URL}/check/${ideaID}`, comment, {
+    .put(`${COMMENT_URL}/check/${commentId}`, {
       headers: { Authorization: `Bearer ${token}` },
     })
+    .then((response) => response.data)
     .catch<Error>(({ response }) => {
-      const error = response.data.error ?? 'Ошибка просмотра комментария'
+      const error = response?.data?.error ?? 'Ошибка просмотра комментария'
       return new Error(error)
     })
 }
 
 const CommentService = {
+  fetchIdeaComments,
   addComment,
   deleteComment,
   checkComment,
