dd416926551ac29f40c517f444e001379b2dc0d8
cap111ffs <mamed.bayramov.04.04@mail.ru>
2023-09-29 19:00:29 +0500

    Сделал функционал для реактивного отображения утвержденных идей экспертами

--- src/components/Modals/IdeaModal/ExpertRatingCalculator.vue
+++ src/components/Modals/IdeaModal/ExpertRatingCalculator.vue
@@ -16,7 +16,8 @@ import useUserStore from '@Store/user/userStore'
 import { Rating } from '@Domain/Idea'
 import RatingService from '@Services/RatingService'
 import { watchImmediate } from '@vueuse/core'
-
+import useRatingStore from '@Store/rating/ratingStore'
+const ratingStore = useRatingStore()
 const props = defineProps<ExperCalculatorProps>()
 const userStore = useUserStore()
 const { user } = storeToRefs(userStore)
@@ -56,11 +57,7 @@ const handleConfirmRating = handleSubmit(async (values) => {
   if (currentUser?.token && props.idea) {
     const { token } = currentUser
     const { id } = props.idea
-    const response = await RatingService.confirmExpertRating(values, id, token)
-
-    if (response instanceof Error) {
-      return
-    }
+    const response = await ratingStore.confirmRating(values, id, token)
 
     if (rating.value) {
       rating.value.confirmed = true
