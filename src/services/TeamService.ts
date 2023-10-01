46384fcb4e8e2fcf7486cf78e44fd440aa7f4c48
andreybyrsa <byrsa2004@bk.ru>
2023-10-01 12:14:49 +0500

    Сделал сервис TeamService.ts

---
+++ src/services/TeamService.ts
@@ -0,0 +1,83 @@
+import axios from 'axios'
+
+import Team from '@Domain/Team'
+import Success from '@Domain/ResponseMessage'
+
+const TEAM_URL = 'http://localhost:3000/api/v1/team'
+
+const getTeams = async (token: string): Promise<Team[] | Error> => {
+  return await axios
+    .get(`${TEAM_URL}/all`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((response) => response.data)
+    .catch(({ response }) => {
+      const error = response?.data?.error ?? 'Ошибка получения команд'
+      return new Error(error)
+    })
+}
+
+const getTeam = async (
+  id: string | string[],
+  token: string,
+): Promise<Team | Error> => {
+  return await axios
+    .get(`${TEAM_URL}/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((response) => response.data)
+    .catch(({ response }) => {
+      const error = response?.data?.error ?? 'Ошибка получения команды'
+      return new Error(error)
+    })
+}
+
+const createTeam = async (team: Team, token: string): Promise<Team | Error> => {
+  return await axios
+    .post(`${TEAM_URL}/create`, team, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((response) => response.data)
+    .catch(({ response }) => {
+      const error = response?.data?.error ?? 'Ошибка создания команды'
+      return new Error(error)
+    })
+}
+
+const updateTeam = async (
+  team: Team,
+  id: string,
+  token: string,
+): Promise<Team | Error> => {
+  return await axios
+    .put(`${TEAM_URL}/update/${id}`, team, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((response) => response.data)
+    .catch(({ response }) => {
+      const error = response?.data?.error ?? 'Ошибка обновления команды'
+      return new Error(error)
+    })
+}
+
+const deleteTeam = async (id: string, token: string): Promise<Success | Error> => {
+  return await axios
+    .delete(`${TEAM_URL}/delete/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((response) => response.data)
+    .catch(({ response }) => {
+      const error = response?.data?.error ?? 'Ошибка удаления команды'
+      return new Error(error)
+    })
+}
+
+const TeamService = {
+  getTeams,
+  getTeam,
+  createTeam,
+  updateTeam,
+  deleteTeam,
+}
+
+export default TeamService
