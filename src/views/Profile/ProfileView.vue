b33a36ac741057c4cc076bcdb5cc96a6bcf0afa9
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2023-10-08 22:53:55 +0500

    Сервисы профиля

--- src/views/Profile/ProfileView.vue
+++ src/views/Profile/ProfileView.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, defineComponent, Ref, onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useForm } from 'vee-validate'
 import { ProfileProps, ProfileEmits } from '@Views/Profile/Profile.types'
 import useCommentsStore from '@Store/comments/commentsStore'
 import RolesTypes from '@Domain/Roles'
+import { useDateFormat } from '@vueuse/core'
+import ProfileService from '@Services/ProfileService'
+import useNotification from '@Hooks/useNotification'
 
 import ModalLayout from '@Layouts/ModalLayout/ModalLayout.vue'
 import Button from '@Components/Button/Button.vue'
@@ -15,9 +18,19 @@ import Collapse from '@Components/Collapse/Collapse.vue'
 import StackCategories from '@Components/Forms/IdeaForm/StackCategories.vue'
 import Select from '@Components/Inputs/Select/Select.vue'
 import ChangeEmailView from '@Views/ChangeEmailView.vue'
+import Input from '@Components/Inputs/Input/Input.vue'
+import { Idea } from '@Domain/Idea'
+import { string } from 'yup'
 
 defineProps<ProfileProps>()
 const emit = defineEmits<ProfileEmits>()
+
+const {
+  notificationOptions,
+  isOpenedNotification,
+  handleOpenNotification,
+  handleCloseNotification,
+} = useNotification()
 
 const commentStore = useCommentsStore()
 const { rsocketIsConnected, closeRsocket } = storeToRefs(commentStore)
@@ -30,6 +43,52 @@ const editSkills = ref(false)
 const skills = ref([])
 
 const isOpenedChangeEmail = ref(false)
+
+const isOpenedChangeInfo = ref(false)
+
+const fileInputRef: Ref<HTMLInputElement | null> = ref(null)
+const imageUrl: Ref<string | null> = ref(null)
+const selectedFile = ref<File | null>(null)
+
+const openFileInput = (): void => {
+  if (fileInputRef.value) {
+    fileInputRef.value.value = ''
+    fileInputRef.value.click()
+  }
+}
+
+const handleFileChange = (event: Event): void => {
+  // const formData = new FormData()
+  // const file = (event.target as HTMLInputElement).files?.[0]
+
+  // formData.append('file', file.toBlob(), file?.name)
+
+  const input = event.target as HTMLInputElement
+  selectedFile.value = input.files[0]
+  const formData = new FormData()
+  formData.append('file', selectedFile.value)
+  if (selectedFile.value) {
+    imageUrl.value = URL.createObjectURL(selectedFile.value)
+  }
+
+  if (user.value?.token) {
+    const { token } = user.value
+    ProfileService.uploadAvatar(formData, token)
+    console.log(formData)
+  }
+}
+
+// const handleFileChange = handleSubmit(async (event: Event) => {
+//   const file = (event.target as HTMLInputElement).files?.[0]
+//   if (file) {
+//     imageUrl.value = URL.createObjectURL(file)
+
+//     if (user.value?.token) {
+//       const { token } = user.value
+//       await ProfileService.uploadAvatar(file, token)
+//     }
+//   }
+// })
 
 const { values, handleSubmit } = useForm({
   validationSchema: {
@@ -53,31 +112,55 @@ const projects = [
     id: '534443557356',
   },
   {
-    name: 'Крутой',
-    discription: 'Очень крутой проект',
+    name: 'Важный проект',
+    discription: 'Очень важный проект',
     id: '5344387656',
   },
 ]
 
-const ideas = [
-  {
-    name: 'Как чистить картошку',
-    discription: 'Новейшие методы чистки картофиля в различных состояниях',
-    id: '4565423',
-  },
-  {
-    name: 'Быстрая покраска морковок',
-    discription: 'Разработка технологии по мнгновенному покрасу моркови в кортофель',
-    id: '5433492225',
-  },
-  {
-    name: 'Иделаьная',
-    discription: 'Ну уж очень классная идея',
-    id: '4578934895348543',
-  },
-]
+// const ideas = [
+//   {
+//     name: 'Как чистить картошку',
+//     discription: 'Новейшие методы чистки картофиля в различных состояниях',
+//     id: '4565423',
+//   },
+//   {
+//     name: 'Быстрая покраска морковок',
+//     discription: 'Разработка технологии по мнгновенному покрасу моркови в кортофель',
+//     id: '5433492225',
+//   },
+//   {
+//     name: 'Иделаьная',
+//     discription: 'Ну уж очень классная идея',
+//     id: '4578934895348543',
+//   },
+// ]
 
-const fieldSubmit = handleSubmit((values) => console.log(values))
+const ideas = ref<Idea[]>([])
+
+onMounted(async () => {
+  const currentUser = user.value
+
+  if (currentUser?.token) {
+    const { token } = currentUser
+    const responseIdeas = await ProfileService.getUserIdeas(token)
+
+    if (responseIdeas instanceof Error) {
+      return handleOpenNotification('error', responseIdeas.message)
+    }
+    ideas.value = responseIdeas
+  }
+
+  if (currentUser?.token) {
+    const { token } = currentUser
+    const responseIdeas = await ProfileService.getAvatar(token)
+
+    if (responseIdeas instanceof Error) {
+      return handleOpenNotification('error', responseIdeas.message)
+    }
+    imageUrl.value = responseIdeas
+  }
+})
 
 function editSkillsHandler() {
   editSkills.value = true
@@ -129,6 +212,36 @@ function handleOpenChangeEmail() {
 function handleCloseChangeEmail() {
   isOpenedChangeEmail.value = false
 }
+
+function handleOpenChangeInfo() {
+  isOpenedChangeInfo.value = true
+}
+
+function handleCloseChangeInfo() {
+  isOpenedChangeInfo.value = false
+}
+
+function getFormattedDate(date: string) {
+  if (date) {
+    const formattedDate = useDateFormat(new Date(date), 'HH:mm DD.MM.YYYY')
+    return formattedDate.value
+  }
+}
+
+// function handleFileUpload(event) {
+//   const file = event.target.files[0]
+// }
+
+// const updateAva = document.getElementById('updateAva')
+
+// const imgAva = document.getElementById('imgAva')
+
+// const imgs = []
+// imgs.push(imgAva)
+
+// updateAva?.addEventListener('click', async (e) => {
+//   fetch('http://localhost:8080/')
+// })
 </script>
 
 <template>
@@ -156,36 +269,58 @@ function handleCloseChangeEmail() {
         </div>
 
         <div class="profile__content">
-          <div class="profile__user bg-white border p-3 rounded-4">
-            <div class="profile__user__head mb-2">
-              <Typography class-name="text-secondary">НЕ В СЕТИ</Typography>
-              <Typography class-name="text-secondary"
-                >Был в сети 17:23 27.09.2023</Typography
-              >
-            </div>
-            <div class="d-flex justify-content-center w-100 mb-2">
-              <img
-                src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ3bieKJm2Fv_x9vA6rcBnqa2-BG93gnmRYM5TT0WxAVgQtfH4L"
-                alt="Avatar"
-                class="avatar"
-              />
-            </div>
-            <div class="profile__user__roles w-100 flex-wrap">
-              <div
-                class="text-center border rounded-pill p-2 bg-light"
-                v-for="role in user.roles"
-                :key="role"
-              >
-                {{ getCurrentRoleRus(role) }}
+          <div class="fixed">
+            <div class="profile__user bg-white border p-3 rounded-4">
+              <div class="profile__user__head mb-2">
+                <Typography class-name="text-secondary">НЕ В СЕТИ</Typography>
+                <Typography class-name="text-secondary"
+                  >Был в сети {{ getFormattedDate(user.lastActivity) }}</Typography
+                >
+              </div>
+              <div class="d-flex justify-content-center w-100 mb-2">
+                <input
+                  ref="fileInputRef"
+                  type="file"
+                  style="display: none"
+                  @input="handleFileChange"
+                  enctype="multipart/form-data"
+                />
+                <Button
+                  class="avatar__button p-1"
+                  @click="openFileInput"
+                >
+                  <img
+                    id="imgAva"
+                    :src="imageUrl"
+                    alt="Avatar"
+                    class="avatar"
+                  />
+                </Button>
+              </div>
+              <div class="profile__user__roles w-100 flex-wrap">
+                <div
+                  class="text-center border rounded-pill p-2 bg-light"
+                  v-for="role in user.roles"
+                  :key="role"
+                >
+                  {{ getCurrentRoleRus(role) }}
+                </div>
               </div>
             </div>
           </div>
 
           <div class="profile__info">
-            <div class="w-100 bg-white border p-3 rounded-4">
+            <div
+              class="w-100 bg-white border p-3 rounded-4"
+              v-if="isOpenedChangeInfo == false"
+            >
               <div class="profile__info__header border-bottom pb-1">
                 <Typography class-name="fs-4 text-primary">Информация</Typography>
-                <Button class-name="border bg-light rounded-pill">изменить</Button>
+                <Button
+                  class-name="border bg-light rounded-pill"
+                  @click="handleOpenChangeInfo"
+                  >изменить</Button
+                >
               </div>
 
               <div class="profile__info__content p-2">
@@ -216,11 +351,59 @@ function handleCloseChangeEmail() {
                     getCurrentRoleRus(user.role)
                   }}</Typography>
                 </div>
+              </div>
+            </div>
+
+            <div
+              class="w-100 bg-white border p-3 rounded-4"
+              v-if="isOpenedChangeInfo == true"
+            >
+              <div class="profile__info__header border-bottom pb-1">
+                <Typography class-name="fs-4 text-primary">Информация</Typography>
+                <Button
+                  class-name="border bg-primary text-light rounded-pill"
+                  @click="handleCloseChangeInfo"
+                  >сохнарить</Button
+                >
+              </div>
+
+              <div class="profile__info__content p-2">
                 <div class="d-grid">
-                  <Typography class-name=" text-secondary">Язык</Typography>
-                  <Typography class-name="fs-5 ms-1 text-muted"
-                    >Не выбран</Typography
+                  <Typography class-name=" text-secondary">Имя</Typography>
+                  <Input
+                    name="firstName"
+                    class-name="rounded-end"
+                    :model-value="user.firstName"
+                    placeholder="Введите ваше имя"
                   >
+                  </Input>
+                </div>
+                <div class="d-grid">
+                  <Typography class-name=" text-secondary">Фамилия</Typography>
+                  <Input
+                    name="lastName"
+                    class-name="rounded-end"
+                    :model-value="user.lastName"
+                    placeholder="Введите вашу фамилию"
+                  >
+                  </Input>
+                </div>
+                <div class="d-grid w-100">
+                  <Typography class-name=" text-secondary">Почта</Typography>
+                  <div class="w-100 d-flex justify-content-between">
+                    <Typography class-name="fs-5 ms-1">{{ user.email }}</Typography>
+                    <Button
+                      class-name="border bg-mutend rounded-pill"
+                      @click="handleOpenChangeEmail"
+                      >изменить почту</Button
+                    >
+                  </div>
+                </div>
+                <div class="d-grid">
+                  <Typography class-name=" text-secondary">Главная роль</Typography>
+                  <Typography class-name="fs-5 ms-1">{{
+                    getCurrentRoleRus(user.role)
+                  }}</Typography>
                 </div>
               </div>
             </div>
@@ -242,14 +425,16 @@ function handleCloseChangeEmail() {
                 v-if="editSkills == false"
               >
                 <div
-                  class="profile__info__content__skills border p-2 fs-5 rounded-4 bg-white"
+                  class="profile__info__content__skills border p-2 fs-5 rounded-4 bg-white text-center"
                   v-for="skill in skills"
                   :key="skill"
                 >
-                  <Typography class-name="fs-5">{{ skill.name }}</Typography>
-                  <Typography :class-name="skillLevel(skill.level)">{{
-                    skill.level
-                  }}</Typography>
+                  <div class="w-100">
+                    <Typography class-name="fs-5">{{ skill.name }}</Typography>
+                    <Typography :class-name="skillLevel(skill.level)">{{
+                      skill.level
+                    }}</Typography>
+                  </div>
                 </div>
               </div>
 
@@ -257,7 +442,8 @@ function handleCloseChangeEmail() {
                 class="profile__info__content p-2"
                 v-if="editSkills == true"
               >
-                <div
+                <StackCategories v-model="skills" />
+                <!-- <div
                   class="profile__info__content__skills border p-2 fs-5 rounded-4 bg-white"
                   v-for="skill in skills"
                   :key="skill"
@@ -275,9 +461,8 @@ function handleCloseChangeEmail() {
                       placeholder="Выберите значение"
                     ></Select>
                   </div>
-                </div>
+                </div> -->
 
-                <StackCategories v-model="skills" />
                 <div class="d-flex">
                   <Button
                     class-name="border bg-primary text-light rounded-pill"
@@ -387,9 +572,10 @@ function handleCloseChangeEmail() {
   }
 
   &__user {
-    width: 33%;
+    width: 25%;
     height: fit-content;
     display: block;
+    position: fixed;
 
     &__head {
       @include flexible(flex-end, center, column);
@@ -439,5 +625,16 @@ function handleCloseChangeEmail() {
   border-radius: 50%;
   width: 250px;
   height: 250px;
+
+  &__button {
+    border-radius: 50%;
+  }
+  &__button:hover {
+    background-color: rgb(92, 112, 221);
+  }
+}
+
+.fixed {
+  width: 33%;
 }
 </style>
