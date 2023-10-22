e206b393787a4505f06d68cf128f5ffd0d021100
cap111ffs <mamed.bayramov.04.04@mail.ru>
2023-10-22 14:55:30 +0500

    Сделал биржу идей

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,6 +26,8 @@ import CommandView from '@Views/CommandView/CommandView.vue'
 
 import DevView from '@Views/DevView.vue'
 
+import IdeasMarket from '@Views/IdeasMarket.vue'
+
 import RoleMiddleware from '@Middlewares/RoleMiddleware.vue'
 import NotificationMiddleware from '@Middlewares/NotificationMiddleware.vue'
 
@@ -165,6 +167,11 @@ const routes: RouteRecordRaw[] = [
     path: '/forgot-password',
     name: 'forgot-password',
     component: ForgotPasswordView,
+  },
+  {
+    path: '/market',
+    name: 'market',
+    component: IdeasMarket,
   },
 ]
 
