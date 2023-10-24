105c474e9877d6949ba5e102682c1e7ea81d7eda
Тимур <116574694+TimurMA@users.noreply.github.com>
2023-10-24 20:12:42 +0500

    фикс

--- src/views/Ideas/IdeasTable.vue
+++ src/views/Ideas/IdeasTable.vue
@@ -1,250 +1,188 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { useDateFormat, useToggle } from '@vueuse/core'
+import { ref, onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
-import { useRouter } from 'vue-router'
+import { watchImmediate } from '@vueuse/core'
 
-import Table from '@Components/Table/Table.vue'
-import { TableColumn } from '@Components/Table/Table.types'
-import Button from '@Components/Button/Button.vue'
-import DropDown from '@Components/DropDown/DropDown.vue'
-import DeleteModal from '@Components/Modals/DeleteModal/DeleteModal.vue'
-import Icon from '@Components/Icon/Icon.vue'
+import LeftSideBar from '@Components/LeftSideBar/LeftSideBar.vue'
+import Typography from '@Components/Typography/Typography.vue'
+import LoadingPlaceholder from '@Components/LoadingPlaceholder/LoadingPlaceholder.vue'
+import FilterBar from '@Components/FilterBar/FilterBar.vue'
 
-import IdeasTableProps from '@Views/Ideas/IdeasView.types'
+import SearchAndFilters from '@Views/Ideas/SearchAndFilters.vue'
+import IdeasTable from '@Views/Ideas/IdeasTable.vue'
+
+import PageLayout from '@Layouts/PageLayout/PageLayout.vue'
 
 import { Idea } from '@Domain/Idea'
-import IdeaStatusTypes from '@Domain/IdeaStatus'
 
 import useUserStore from '@Store/user/userStore'
 import useIdeasStore from '@Store/ideas/ideasStore'
 
-import getStatus from '@Utils/getStatus'
-
-const props = defineProps<IdeasTableProps>()
-
-const availableStatus = getStatus()
-
 const userStore = useUserStore()
 const { user } = storeToRefs(userStore)
 
-const router = useRouter()
-
-const [isSorted, setIsSorted] = useToggle(true)
-
 const ideaStore = useIdeasStore()
+const { ideas } = storeToRefs(ideaStore)
 
-const gridColumns: TableColumn[] = [
+const ideasData = ref<Idea[]>([])
+
+const searchedValue = ref('')
+const selectedFilters = ref<string[]>([])
+
+const isLoading = ref(true)
+
+const c1 = ref([])
+const c2 = ref()
+
+const currrentFilters = [
   {
-    key: 'name',
-    label: 'Название',
-    className: 'col-3 justify-content-start text-start',
+    category: 'Идеи',
+    choices: [
+      { label: 'Мои идеи', value: { name: 1, value: 2 } },
+      { label: 'Идеи конкурентов', value: 2 },
+      { label: 'Идеи организации', value: '3' },
+    ],
+    refValue: c1,
+
+    isUniqueChoice: false,
   },
   {
-    key: 'status',
-    label: 'Статус',
-    className: 'col-2',
-    getFormat: getTranslatedStatus,
-  },
-  {
-    key: 'createdAt',
-    label: 'Дата создания',
-    getFormat: getFormattedDate,
-    click: () => sortDateCreated(props.ideas),
-  },
-  {
-    key: 'modifiedAt',
-    label: 'Дата изменения',
-    getFormat: getFormattedDate,
-    click: () => sortDateModified(props.ideas),
-  },
-  {
-    key: 'preAssessment',
-    label: 'Оценка',
-    getFormat: (value) => value.toFixed(1),
-    getStyle: getRatingColor,
-  },
-  {
-    key: 'rating',
-    label: 'Рейтинг',
-    getStyle: getRatingColor,
-    getFormat: getFloorRating,
-    click: () => sortRating(props.ideas),
+    category: 'Статус',
+    choices: [
+      { label: 'На рассмотрении', value: { name: 1, value: 2 } },
+      { label: 'На согласовании', value: '5' },
+      { label: 'На утверждении', value: '6' },
+    ],
+
+    refValue: c2,
+
+    isUniqueChoice: true,
   },
 ]
 
-function sortRating(ideas: Idea[]) {
-  ideas.sort((a, b) => {
-    if (a.rating == b.rating) {
-      const A = new Date(a.createdAt).getTime()
-      const B = new Date(b.createdAt).getTime()
-      return A - B
-    } else return isSorted.value ? a.rating - b.rating : b.rating - a.rating
-  })
-  setIsSorted()
-}
+const sideBarFilters = ref([])
 
-function sortDateModified(ideas: Idea[]) {
-  ideas.sort((a, b) => {
-    const A = new Date(a.modifiedAt).getTime()
-    const B = new Date(b.modifiedAt).getTime()
-    return isSorted.value ? B - A : A - B
-  })
-  setIsSorted()
-}
-
-function sortDateCreated(ideas: Idea[]) {
-  ideas.sort((a, b) => {
-    const A = new Date(a.createdAt).getTime()
-    const B = new Date(b.createdAt).getTime()
-    return isSorted.value ? B - A : A - B
-  })
-  setIsSorted()
-}
-
-function getTranslatedStatus(status: IdeaStatusTypes) {
-  return availableStatus.translatedStatus[status]
-}
-
-function getFormattedDate(date: string) {
-  if (date) {
-    const formattedDate = useDateFormat(new Date(date), 'DD.MM.YYYY')
-    return formattedDate.value
-  }
-}
-
-function getFloorRating(rating: number) {
-  return rating.toFixed(1)
-}
-
-function getRatingColor(rating: number) {
-  if (rating >= 4.0) {
-    return 'text-success'
-  }
-  if (rating < 4.0 && rating >= 3.0) {
-    return 'text-warning'
-  }
-  return 'text-danger'
-}
-
-const ideaId = ref<string>('')
-const isOpenedIdeaDeleteModal = ref(false)
-
-function handleOpenDeleteModal(id: string) {
-  ideaId.value = id
-  isOpenedIdeaDeleteModal.value = true
-}
-function handleCloseDeleteModal() {
-  isOpenedIdeaDeleteModal.value = false
-}
-
-async function handleDeleteIdea() {
+onMounted(async () => {
   const currentUser = user.value
+
   if (currentUser?.token) {
     const { token } = currentUser
-    await ideaStore.deleteInitiatorIdea(ideaId.value, token)
+
+    await ideaStore.fetchIdeas(token)
+
+    ideas.value.sort((a, b) => {
+      if (a.rating == b.rating) {
+        const A = new Date(a.createdAt).getTime()
+        const B = new Date(b.createdAt).getTime()
+        return A - B
+      } else return b.rating - a.rating
+    })
+
+    ideasData.value = ideas.value
+
+    isLoading.value = false
   }
+})
+
+watchImmediate(
+  () => user.value?.role,
+  () => {
+    switch (user.value?.role) {
+      case 'EXPERT':
+        return (selectedFilters.value = ['ON_CONFIRMATION'])
+      case 'PROJECT_OFFICE':
+        return (selectedFilters.value = ['ON_APPROVAL'])
+      case 'INITIATOR':
+        return (selectedFilters.value = [user.value.email])
+      default:
+        selectedFilters.value = []
+    }
+  },
+)
+
+function filterIdeas(ideasData: Idea[]) {
+  if (selectedFilters.value.length) {
+    const dataFilter: Idea[] = []
+    ideasData?.forEach(
+      (elem) =>
+        selectedFilters.value?.every((filter) =>
+          Object.values(elem).includes(filter),
+        ) && dataFilter.push(elem),
+    )
+    return dataFilter
+  } else false
 }
 
-function checkButtonDelete(initiator: string) {
-  return (user.value?.role == 'INITIATOR' && user.value.email == initiator) ||
-    user.value?.role == 'ADMIN'
-    ? true
-    : false
-}
-
-function checkButtonEdit(initiator: string, status: IdeaStatusTypes) {
-  return (user.value?.role == 'INITIATOR' &&
-    user.value.email == initiator &&
-    (status == 'NEW' || status == 'ON_EDITING')) ||
-    user.value?.role == 'ADMIN'
-    ? true
-    : false
-}
-
-function checkMark(row: Idea) {
-  const currentRole = user.value?.role
-  const currentStatusIdea = row.status
-  const currentInitiatorIdea = row.initiator
-  const currentEmail = user.value?.email
-  return currentRole == 'INITIATOR' &&
-    (currentStatusIdea == 'NEW' || currentStatusIdea == 'ON_EDITING') &&
-    currentInitiatorIdea == currentEmail
-    ? true
-    : currentRole == 'PROJECT_OFFICE' && currentStatusIdea == 'ON_APPROVAL'
-    ? true
-    : currentRole == 'EXPERT' && currentStatusIdea == 'ON_CONFIRMATION'
-    ? true
-    : false
-}
+const filters = [
+  {
+    label: 'Мои идеи',
+    value: user.value?.email,
+  },
+  {
+    label: 'На согласованнии',
+    value: 'ON_APPROVAL',
+  },
+  {
+    label: 'На утверждении',
+    value: 'ON_CONFIRMATION',
+  },
+]
 </script>
 
 <template>
-  <Table
-    :columns="gridColumns"
-    :data="ideas"
-    :search-value="searchedValue"
-  >
-    <template #actions="{ item }: { item: Idea }">
-      <Button
-        type="button"
-        class-name="btn-primary"
-        prepend-icon-name="bi bi-list"
-        v-dropdown="'ideasTable'"
-      ></Button>
-      <DropDown id="ideasTable">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item list-group-item-action p-1">
-            <button
-              class="w-100 text-start"
-              @click="router.push(`/ideas/list/${item.id}`)"
-            >
-              Просмотреть идею
-            </button>
-          </li>
-          <li
-            v-if="checkButtonEdit(item.initiator, item.status)"
-            class="list-group-item list-group-item-action p-1"
-          >
-            <button
-              class="w-100 text-start"
-              @click="router.push(`/ideas/edit/${item.id}`)"
-            >
-              Редактировать
-            </button>
-          </li>
-          <li
-            v-if="checkButtonDelete(item.initiator)"
-            class="list-group-item list-group-item-action p-1"
-          >
-            <button
-              class="w-100 text-start text-danger"
-              @click="handleOpenDeleteModal(item.id)"
-            >
-              Удалить
-            </button>
-          </li>
-        </ul>
-      </DropDown>
+  <PageLayout content-class-name="ideas-page__content p-3">
+    <template #leftSideBar>
+      <LeftSideBar />
     </template>
-    <template
-      v-if="user?.role != 'ADMIN'"
-      #icon="{ item }: { item: Idea }"
-    >
-      <Icon
-        v-if="checkMark(item)"
-        class-name="bi bi-circle-fill text-success bg-transparent fs-6"
-      />
-      <Icon
-        v-else
-        class-name="bi bi-circle-fill text-secondary bg-transparent fs-6 opacity-25"
-      />
-    </template>
-  </Table>
 
-  <DeleteModal
-    :is-opened="isOpenedIdeaDeleteModal"
-    @close-modal="handleCloseDeleteModal"
-    @delete="handleDeleteIdea"
-  />
+    <template #content>
+      <div class="ideas-page__table-and-filter w-100">
+        <router-view></router-view>
+
+        <Typography class-name="fs-2 text-primary w-75">Список идей</Typography>
+
+        <div class="w-100 d-flex justify-content-center">
+          <SearchAndFilters
+            :filtersData="filters"
+            v-model:searchedValue="searchedValue"
+            v-model:selectedFilters="selectedFilters"
+          />
+        </div>
+
+        <template v-if="isLoading">
+          <LoadingPlaceholder
+            v-for="value in 3"
+            :key="value"
+            height="medium"
+          />
+        </template>
+
+        <IdeasTable
+          v-else
+          :ideas="filterIdeas(ideasData) || ideasData"
+          :searched-value="searchedValue"
+        />
+      </div>
+
+      {{ c1 }}
+      {{ c2 }}
+
+      <FilterBar
+        title="Фильтрация по идеям"
+        :filters="currrentFilters"
+        v-model="sideBarFilters"
+      />
+    </template>
+  </PageLayout>
 </template>
+
+<style lang="scss">
+.ideas-page {
+  &__content {
+    @include flexible(start, start);
+  }
+  &__table-and-filter {
+    @include flexible(center, start, column, $gap: 12px);
+  }
+}
+</style>
