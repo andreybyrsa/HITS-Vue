cf31b14365fac41388bf0bf4f627b5ad08e5dcaa
andreybyrsa <byrsa2004@bk.ru>
2023-10-29 16:16:34 +0500

    Сделал новую таблицу

--- src/components/Table/Table.types.ts
+++ src/components/Table/Table.types.ts
@@ -1,17 +1,63 @@
-interface TableColumn {
-  key: string
+import { Filter } from '@Components/FilterBar/FilterBar.types'
+
+type KeyValueFunction<T, K extends keyof T> = (
+  value: T[K],
+  ...args: number[]
+) => void
+
+type TypedStyleFunction<T> = { [K in keyof T]-?: KeyValueFunction<T, K> }[keyof T]
+type TypedFormatFunction<T> = { [K in keyof T]-?: KeyValueFunction<T, K> }[keyof T]
+
+type ColumnSize =
+  | 'col'
+  | 'col-1'
+  | 'col-2'
+  | 'col-3'
+  | 'col-4'
+  | 'col-5'
+  | 'col-6'
+  | 'col-7'
+  | 'col-8'
+
+interface CheckedDataAction<DataType> {
   label: string
-  icon?: string
   className?: string
-  click?: () => void
-  getStyle?: (value: any) => string
-  getFormat?: (value: any) => string | number | undefined
+  statement?: boolean
+  click: (values: DataType[]) => void
 }
 
-interface TableProps {
-  columns: TableColumn[]
-  data: any[]
-  searchValue?: string
+interface DropdownMenuAction<DataType> {
+  label: string
+  className?: string
+  statement?: (data: DataType) => boolean
+  click: (value: DataType) => void
 }
 
-export { TableProps, TableColumn }
+interface TableColumn<DataType> {
+  key: keyof DataType
+  label: string
+  size?: ColumnSize
+  contentClassName?: string
+  getRowCellStyle?: TypedStyleFunction<DataType>
+  getRowCellFormat?: TypedFormatFunction<DataType>
+  headerCellClick?: () => void
+  rowCellClick?: (value: DataType) => void
+}
+
+interface TableProps<DataType> {
+  columns: TableColumn<DataType>[]
+  data: DataType[]
+  searchBy?: keyof DataType
+  filters?: Filter[]
+  checkedDataActions?: CheckedDataAction<DataType>[]
+  dropdownActionsMenu?: DropdownMenuAction<DataType>[]
+}
+
+export {
+  TableProps,
+  TableColumn,
+  TypedFormatFunction,
+  TypedStyleFunction,
+  CheckedDataAction,
+  DropdownMenuAction,
+}
