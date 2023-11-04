29e843c5f370e88d976a85dfbb67359b4a4422f7
Тимур <116574694+TimurMA@users.noreply.github.com>
2023-11-04 18:33:51 +0500

    удалил старый радар чарт

--- src/components/Modals/TeamModal/TeamModal.types.ts
+++ src/components/Modals/TeamModal/TeamModal.types.ts
@@ -1,14 +1,5 @@
-import { Skill } from '@Domain/Skill'
 import Team from '@Domain/Team'
 import TeamMember from '@Domain/TeamMember'
-
-interface ProfileSkillChartsProps {
-  team: Team
-}
-
-interface ProfileSkillChartsEmits {
-  (event: 'filter', skill: Skill): void
-}
 
 interface TeamStuffProps {
   team: Team
@@ -23,10 +14,4 @@ interface TeamDescriptionEmits {
   (event: 'closeModal'): void
 }
 
-export {
-  ProfileSkillChartsProps,
-  ProfileSkillChartsEmits,
-  TeamStuffProps,
-  TeamDescriptionProps,
-  TeamDescriptionEmits,
-}
+export { TeamStuffProps, TeamDescriptionProps, TeamDescriptionEmits }
