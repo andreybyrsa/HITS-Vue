b161acfc709c37dc81ad295b43b4990dfb5ee3c0
andreybyrsa <byrsa2004@bk.ru>
2023-11-19 21:03:42 +0500

    Вернул массив с компаниями

--- src/components/Forms/IdeaForm/CustomerAndContact.vue
+++ src/components/Forms/IdeaForm/CustomerAndContact.vue
@@ -20,76 +20,144 @@ import CompanyService from '@Services/CompanyService'
 const props = defineProps<CustomerAndContact>()
 const emit = defineEmits<CustomerAndContactEmits>()
 
-const userStore = useUserStore()
-const { user } = storeToRefs(userStore)
-const notificationsStore = useNotificationsStore()
+// const userStore = useUserStore()
+// const { user } = storeToRefs(userStore)
+// const notificationsStore = useNotificationsStore()
 
-const customers = ref<Company[]>([])
-const currentCompanies = computed(() =>
-  customers.value.map((company) => company.name),
-)
+// const customers = ref<Company[]>([])
+// const currentCompanies = computed(() =>
+//   customers.value.map((company) => company.name),
+// )
 
-const currentCompanyContacts = computed(() => {
-  const selectedCompany = customers.value.find(
-    (company) => company.name === props.idea.customer,
-  )
-  return selectedCompany ? getContactPersonsByCompany(props.idea.customer) : []
-})
+// const currentCompanyContacts = computed(() => {
+//   const selectedCompany = customers.value.find(
+//     (company) => company.name === props.idea.customer,
+//   )
+//   return selectedCompany ? getContactPersonsByCompany(props.idea.customer) : []
+// })
 
-async function setCompaniesByRole(token: string, role: RolesTypes) {
-  const response = await CompanyService.getCompanies(token)
+// async function setCompaniesByRole(token: string, role: RolesTypes) {
+//   const response = await CompanyService.getCompanies(token)
 
-  if (response instanceof Error) {
-    return notificationsStore.createSystemNotification('Система', response.message)
-  }
+//   if (response instanceof Error) {
+//     return notificationsStore.createSystemNotification('Система', response.message)
+//   }
 
-  if (role === 'ADMIN') {
-    customers.value = response
-  } else {
-    customers.value = response.filter((company) => company.name === 'ВШЦТ')
-  }
-}
+//   if (role === 'ADMIN') {
+//     customers.value = response
+//   } else {
+//     customers.value = response.filter((company) => company.name === 'ВШЦТ')
+//   }
+// }
 
-onMounted(async () => {
-  const currentUser = user.value
+// onMounted(async () => {
+//   const currentUser = user.value
 
-  if (currentUser?.token && currentUser.role) {
-    const { token, id, role } = currentUser
+//   if (currentUser?.token && currentUser.role) {
+//     const { token, id, role } = currentUser
 
-    if (role !== 'ADMIN') {
-      const response = await CompanyService.getOwnerCompanies(id, token)
+//     if (role !== 'ADMIN') {
+//       const response = await CompanyService.getOwnerCompanies(id, token)
 
-      if (response instanceof Error) {
-        return notificationsStore.createSystemNotification(
-          'Система',
-          response.message,
-        )
-      }
+//       if (response instanceof Error) {
+//         return notificationsStore.createSystemNotification(
+//           'Система',
+//           response.message,
+//         )
+//       }
 
-      if (response.length) {
-        customers.value = response
-      } else {
-        setCompaniesByRole(token, role)
-      }
-    } else {
-      setCompaniesByRole(token, role)
-    }
-  }
-})
+//       if (response.length) {
+//         customers.value = response
+//       } else {
+//         setCompaniesByRole(token, role)
+//       }
+//     } else {
+//       setCompaniesByRole(token, role)
+//     }
+//   }
+// })
 
-const getContactPersonsByCompany = (company: string): string[] => {
-  const selectedCompany = customers.value.find((option) => option.name === company)
-  return selectedCompany
-    ? selectedCompany.users.map((user) => `${user.firstName} ${user.lastName}`)
-    : []
+// const getContactPersonsByCompany = (company: string): string[] => {
+//   const selectedCompany = customers.value.find((option) => option.name === company)
+//   return selectedCompany
+//     ? selectedCompany.users.map((user) => `${user.firstName} ${user.lastName}`)
+//     : []
+// }
+
+// function handleCustomerChange(selectedCompany: string) {
+//   const currentContacts = getContactPersonsByCompany(selectedCompany)
+//   if (currentContacts) {
+//     const { contactPerson } = props.idea
+
+//     const currentContactPerson = currentCompanyContacts.value.includes(contactPerson)
+//       ? contactPerson
+//       : currentContacts[0]
+
+//     emit('set-value', 'contactPerson', currentContactPerson)
+//   }
+// }
+
+const customers = ref([
+  { contacts: ['ВШЦТ'], company: 'ВШЦТ' },
+  { contacts: ['Красильников Сергей Владимирович'], company: 'ГК АВТОГРАД' },
+  { contacts: ['Зайко Антон Алексеевич'], company: 'Студия дизайна PavlovaWeb' },
+  {
+    contacts: [
+      'Андрей Дрыгни (HR)',
+      'Асташова Ксения (портфель проектов)',
+      'Качанова Яна (проектный офис) ',
+    ],
+    company: 'ООО Цифровое строительство',
+  },
+  {
+    contacts: ['Томилов Алексей Александрович'],
+    company: 'ООО "Ребренди Консалтинг"',
+  },
+  { contacts: ['Громов Игорь Сергеевич'], company: 'ЗапСибНИИГГ' },
+  {
+    contacts: ['Беляков Дмитрий Константинович'],
+    company: 'Базис-моторс',
+  },
+  {
+    contacts: ['Беляков Дмитрий Константинович'],
+    company: 'ООО "Бизнес-Аудит"',
+  },
+  {
+    contacts: ['Букреев Павел Александрович'],
+    company: '1C-Медицина-Регион',
+  },
+  {
+    contacts: ['Золотов Анатолий Леонидович'],
+    company: 'Tip-Top Studio',
+  },
+  {
+    contacts: ['Николаев Алексей Андреевич', 'Исаев Сергей Викторович'],
+    company: 'ООО "Зуммер"',
+  },
+  {
+    contacts: ['Галагуш Артем Василич'],
+    company: 'ООО Бизнес Технологии',
+  },
+  {
+    contacts: ['Калашников Вячеслав Сергеевич'],
+    company: 'ООО "ВОК"',
+  },
+])
+
+const currentCompanies = ref(customers.value.map((option) => option.company))
+const currentCompanyContacts = ref<string[]>([])
+
+function getContactPersonsByCompany(company: string) {
+  return customers.value.find((option) => option.company === company)
 }
 
 function handleCustomerChange(selectedCompany: string) {
-  const currentContacts = getContactPersonsByCompany(selectedCompany)
+  const currentContacts = getContactPersonsByCompany(selectedCompany)?.contacts
   if (currentContacts) {
+    currentCompanyContacts.value = currentContacts
     const { contactPerson } = props.idea
 
-    const currentContactPerson = currentCompanyContacts.value.includes(contactPerson)
+    const currentContactPerson = currentContacts.includes(contactPerson)
       ? contactPerson
       : currentContacts[0]
 
