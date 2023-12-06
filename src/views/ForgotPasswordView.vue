b596f1577cb08a3e221bf2713976b850aa89f735
andreybyrsa <byrsa2004@bk.ru>
2023-12-06 21:37:25 +0500

    Добавил индикатор загрузки в систему аутентификации

--- src/views/ForgotPasswordView.vue
+++ src/views/ForgotPasswordView.vue
@@ -1,19 +1,30 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { useForm } from 'vee-validate'
+
 import Button from '@Components/Button/Button.vue'
 import Input from '@Components/Inputs/Input/Input.vue'
-import PageLayout from '@Layouts/PageLayout/PageLayout.vue'
 import Typography from '@Components/Typography/Typography.vue'
 import NewPasswordModal from '@Components/Modals/NewPasswordModal/NewPasswordModal.vue'
+
+import PageLayout from '@Layouts/PageLayout/PageLayout.vue'
 import FormLayout from '@Layouts/FormLayout/FormLayout.vue'
+
 import { RecoveryData } from '@Domain/Invitation'
+
 import InvitationService from '@Services/InvitationService'
+
 import useNotificationsStore from '@Store/notifications/notificationsStore'
+
 import Validation from '@Utils/Validation'
+
+const notificationsStore = useNotificationsStore()
+
 const isOpenedModal = ref(false)
 const authKey = ref('')
-const notificationsStore = useNotificationsStore()
+
+const isLoading = ref(false)
+
 const { values, handleSubmit } = useForm<RecoveryData>({
   validationSchema: {
     email: (value: string) =>
@@ -21,10 +32,14 @@ const { values, handleSubmit } = useForm<RecoveryData>({
   },
 })
 const sendRevoveryEmail = handleSubmit(async (values) => {
+  isLoading.value = true
   const response = await InvitationService.sendRecoveryEmail(values)
+  isLoading.value = false
+
   if (response instanceof Error) {
     return notificationsStore.createSystemNotification('Система', response.message)
   }
+
   authKey.value = response
   isOpenedModal.value = true
   notificationsStore.createSystemNotification(
@@ -33,6 +48,7 @@ const sendRevoveryEmail = handleSubmit(async (values) => {
   )
 })
 </script>
+
 <template>
   <PageLayout content-class-name="forgot-password-page__content">
     <template #content>
@@ -40,20 +56,24 @@ const sendRevoveryEmail = handleSubmit(async (values) => {
         <Typography class-name="fs-3 text-primary text-center">
           Восстановление пароля
         </Typography>
+
         <Input
-          type="text"
+          type="email"
           name="email"
           class-name="rounded-end"
           placeholder="Введите email"
           prepend="@"
         />
+
         <Button
           type="submit"
           variant="primary"
+          :is-loading="isLoading"
           @click="sendRevoveryEmail"
         >
           Отправить
         </Button>
+
         <NewPasswordModal
           :is-opened="isOpenedModal"
           :email="values.email"
