9b960ef0899eb8ecf71ca9a74a73c43cdd1b7daa
shadowoem <bessonovads@yandex.ru>
2023-12-14 17:44:29 +0500

    добавлены все сервисы для бирж

--- src/services/MarketsService.ts
+++ src/services/MarketsService.ts
@@ -1,10 +1,15 @@
-import { Market } from '@Domain/Market'
+import { Market, MarketStatus } from '@Domain/Market'
+import Success from '@Domain/ResponseMessage'
+
+import useUserStore from '@Store/user/userStore'
 
 import defineAxios from '@Utils/defineAxios'
 import getMocks from '@Utils/getMocks'
+import getAbortedSignal from '@Utils/getAbortedSignal'
 
 const marketAxios = defineAxios(getMocks().market)
 
+// --- GET --- ///
 const fetchMarkets = async (token: string): Promise<Market[] | Error> => {
   return await marketAxios
     .get('/market/all', {
@@ -17,8 +22,88 @@ const fetchMarkets = async (token: string): Promise<Market[] | Error> => {
     })
 }
 
+// --- POST --- ///
+const createMarket = async (
+  market: Market,
+  token: string,
+): Promise<Market | Error> => {
+  return await marketAxios
+    .post('/market/create', market, {
+      headers: { Authorization: `Bearer ${token}` },
+      signal: getAbortedSignal(useUserStore().checkIsExpiredToken),
+    })
+    .then((response) => response.data)
+    .catch(({ response }) => {
+      const error = response?.data?.error ?? 'Ошибка создания биржи'
+      return new Error(error)
+    })
+}
+
+// --- DELETE --- ///
+const deleteMarket = async (id: string, token: string): Promise<Success | Error> => {
+  return await marketAxios
+    .delete(
+      `/market/delete/${id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+        signal: getAbortedSignal(useUserStore().checkIsExpiredToken),
+      },
+      { params: { id } },
+    )
+    .then((response) => response.data)
+    .catch(({ response }) => {
+      const error = response?.data?.error ?? 'Ошибка удаления биржи'
+      return new Error(error)
+    })
+}
+
+// --- PUT --- ///
+const updateMarket = async (
+  market: Market,
+  id: string,
+  token: string,
+): Promise<Success | Error> => {
+  return await marketAxios
+    .put<Success>(
+      `/market/update/${id}`,
+      market,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+        signal: getAbortedSignal(useUserStore().checkIsExpiredToken),
+      },
+      { params: { id }, responseData: { success: 'Успешное обновление биржи' } },
+    )
+    .then((response) => response.data)
+    .catch(({ response }) => {
+      const error = response?.data?.error ?? 'Ошибка редактирования биржи'
+      return new Error(error)
+    })
+}
+
+const updateMarketStatus = async (
+  id: string,
+  status: MarketStatus,
+  token: string,
+): Promise<Success | Error> => {
+  return await marketAxios
+    .putNoRequestBody<Success>(
+      `/market/${id}/${status}`,
+      { headers: { Authorization: `Bearer ${token}` } },
+      { params: { id } },
+    )
+    .then((response) => response.data)
+    .catch(({ response }) => {
+      const error = response?.data?.error ?? 'Ошибка загрузки статуса биржи'
+      return new Error(error)
+    })
+}
+
 const MarketService = {
   fetchMarkets,
+  createMarket,
+  deleteMarket,
+  updateMarket,
+  updateMarketStatus,
 }
 
 export default MarketService
