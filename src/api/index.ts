1511e55e1d032235a4798f74eaa326b376449c3c
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-02-28 20:02:00 +0500

    add signal

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 import { API_URL } from '@Main'
+import useUserStore from '@Store/user/userStore'
+import getAbortedSignal from '@Utils/getAbortedSignal'
 
 export const api: AxiosInstance = axios.create({
   baseURL: API_URL,
@@ -8,5 +10,6 @@ export const api: AxiosInstance = axios.create({
 
 api.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+  config.signal = getAbortedSignal(useUserStore().checkIsExpiredToken)
   return config
 })
