25605364cd672970ebe48acf60eacafc134652ec
PolyanskiyDanil <danilp.2005@mail.ru>
2024-04-20 18:49:37 +0500

    test

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+echo "Сообщение: $0 <Сообщение коммита>"
+exit 1
+fi
+cd "$($dirname "$0")"
+git add .
+git commit -m "$1"
+current_branch=$(git rev-parse -- addrev-ref HEAD)
+git checkout task-information-tab
+git merge $current_branch
+git push origin task-information-tab
