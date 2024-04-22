327b9ab3e567bff4aaabf01246064316aba31763
PolyanskiyDanil <danilp.2005@mail.ru>
2024-04-22 18:46:15 +0500

    test6

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+echo "Сообщение: $0 <Сообщение коммита>"
+exit 1
+fi
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 git add .
-echo "Введите сообщение коммита"
-read commit
 git commit -m "$commit"
 git checkout task-information-tab
 git merge $current_branch
