36da54030dd33c7fbdec3f2dc7f39133869913e6
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-06-04 21:33:24 +0500

    is-passed --> is-pass

--- src/domain/Quest.ts
+++ src/domain/Quest.ts
@@ -64,7 +64,7 @@ export interface QuestTeamsProgress {
 
 export interface QuestStat {
   idQuest: string
-  isPassed: boolean
+  isPass: boolean
   idQuestTemplate: string
   name: string
   progress: string
