a5d6f25ee4ae6cfa109e92516803cca021e297fa
kivaX9 <kirill.vlasov.05@inbox.tu>
2024-06-13 17:54:34 +0500

    dockerfile 5

--- replace_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Используем `envsubst` для замены переменных окружения в index.html
-envsubst '\$API_URL \$VUE_APP_OAUTH_REDIRECT_URI \$VUE_APP_OAUTH_CLIENT_ID \$VUE_APP_OAUTH_AUTH_HEADER \$VUE_APP_OAUTH_URL' < /app/public/index.html > /app/public/index.html.tmp && mv /app/public/index.html.tmp /app/public/index.html
-
-# Запустим Nginx
-exec "$@"
